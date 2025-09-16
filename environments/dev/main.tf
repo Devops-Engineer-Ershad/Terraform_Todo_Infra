e1ce3416a2167b8dde911107aa8ef73ec7f90473
ershadalam <ershadalams150@gmail.com>
2025-09-16 16:06:17 +0530

    new code added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -22,7 +22,7 @@ module "resource_group" {
 module "container_registry" {
   depends_on = [ module.resource_group ]
   source = "../../modules/azurerm_container_registry"
-  acr_name = "todocontainerregistry"
+  acr_name = "todoacr_01"
   rg_name = "rg-todo-dev"
   location = "East US"
   tags = local.common_tags
