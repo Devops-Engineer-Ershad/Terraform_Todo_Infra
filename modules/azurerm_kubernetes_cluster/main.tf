41c3b63c5926af26a9682be7e3c23270f131ca67
devopsengineer-ershad <ershadalams150@gmail.com>
2025-10-20 13:13:55 +0530

    Increase default node count from 1 to 2

--- modules/azurerm_kubernetes_cluster/main.tf
+++ modules/azurerm_kubernetes_cluster/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
 
     default_node_pool {
         name       = "default"
-        node_count = 1
+        node_count = 2
         vm_size    = "standard_a2m_v2"
     }
 
